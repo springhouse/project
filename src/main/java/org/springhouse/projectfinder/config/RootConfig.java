7c3c1b95ae5359c039ce72ca7b7c72bb7522cf6d
Choi Seungeon <chou610@naver.com>
2019-09-01 02:29:26 +0900

    [Update] 프로젝트 뼈대

---
+++ src/main/java/org/springhouse/projectfinder/config/RootConfig.java
@@ -0,0 +1,38 @@
+package org.springhouse.projectfinder.config;
+
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+@ComponentScan(basePackages = "org.springhouse.*")
+@MapperScan(basePackages = {"com.forestvue.mapper"})
+public class RootConfig {
+    @Bean
+    public DataSource dataSource(){
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+        hikariConfig.setJdbcUrl("jdbc:log4jdbc:mysql://localhost:3306/springhouse?useSSL=false");
+        hikariConfig.setUsername("springhouse");
+        hikariConfig.setPassword("springhouse");
+        HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+        return dataSource;
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+        sqlSessionFactory.setDataSource(dataSource());
+        return (SqlSessionFactory) sqlSessionFactory.getObject();
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: chou6
+  Date: 2019-09-01
+  Time: 오전 1:16
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello</title>
+</head>
+<body>
+    Hello world!
+</body>
+</html>
